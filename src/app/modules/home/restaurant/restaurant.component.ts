a4a724ea087a642e1877df3bbcad0116c9fcd52f
Soulnomad <anthoniagreda@gmail.com>
2023-01-25 18:33:37 -0400

    alert

--- src/app/modules/home/restaurant/restaurant.component.ts
+++ src/app/modules/home/restaurant/restaurant.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-restaurant',
@@ -8,7 +9,40 @@ import { Component } from '@angular/core';
 
 
 
-export class RestaurantComponent {
+export class RestaurantComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+  constructor(){
+  }
+  timerInterval: any
+  showModal(){
+    Swal.fire({
+      timer: 2000,
+      timerProgressBar: true,
+      imageUrl: '../../../../assets/img/modal.jpg',
+      imageWidth: 400,
+      imageHeight: 500,
+      
+      didOpen: () => {
+        Swal.showLoading()
+        
+        this.timerInterval = setInterval(() => {
+        }, 100)
+      },
+      willClose: () => {
+        clearInterval(this.timerInterval)
+      }
+    }).then((result) => {
+      /* Read more about handling dismissals below */
+      if (result.dismiss === Swal.DismissReason.timer) {
+        console.log('I was closed by the timer')
+      }
+    })
+    
+    
+  }
 
 }
 
